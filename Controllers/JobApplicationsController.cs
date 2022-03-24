8da778ca0d9ddaa68b5dc3584de901540f43b9e4
bragalucas1 <lucas.b.moura@ufv.br>
2022-03-23 22:17:59 -0300

    .Adicionar arquivos de projeto.

---
+++ Controllers/JobApplicationsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobApi.Entities;
+using JobApi.Models;
+using JobApi.Persistence;
+
+
+namespace JobApi.Controllers
+{
+    public class JobApplicationsController:ControllerBase
+    {
+        private readonly DevJobsContext _context;
+ 
+            
+            public JobApplicationsController(DevJobsContext context)
+            {
+                _context = context;
+            }
+
+            // POST api/job-vacancies/4/applications
+            [HttpPost]
+            public IActionResult Post(int id, AddJobApplicationInputModel model)
+            {
+                var jobVacancy = _context.JobVacancies
+                    .SingleOrDefault(jv => jv.Id == id);
+
+                if (jobVacancy == null)
+                    return NotFound();
+
+                var application = new JobApplication(
+                    model.ApplicantName,
+                    model.ApplicantEmail,
+                    id
+                );
+
+                _context.JobApplications.Add(application);
+                _context.SaveChanges();
+
+                return NoContent();
+            }
+        }
+    }
+}
